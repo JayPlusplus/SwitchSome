8c46eef51e50ffa76272494cefd29df48b421461
Jay Wu <wu274319426@gmail.com>
2017-06-21 13:51:05 -0400

    switchSome

---
+++ src/switchMonth/SwitchSome.java
@@ -0,0 +1,26 @@
+package switchMonth;
+
+public class SwitchSome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int numberofdays=28;
+		switch (numberofdays) {
+		case 28: 
+			System.out.println("Feburary");
+			break;
+		case 30:
+			System.out.println("September April June November");
+			break;
+		case 31:
+			System.out.println("Jan Mar May July Aug Oct Dec");
+			break;
+			default: 
+				
+		}
+		
+
+	}
+	
+
+}
